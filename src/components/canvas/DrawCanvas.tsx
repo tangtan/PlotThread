0c967d4c8c3c6c8619df313f2dfbbf0ab7a3433d
zangfenziang <zangfenziang@126.com>
2019-07-15 10:57:19 +0800

    add order & straighten animation

--- src/components/canvas/DrawCanvas.tsx
+++ src/components/canvas/DrawCanvas.tsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Path, Item, Point, view, project } from 'paper';
 import { StateType, DispatchType } from '../../types';
 import ZoomCanvas from './ZoomCanvas';
-import { storyflow } from 'story-flow';
+import { iStoryline } from 'story-flow';
 import { xml } from 'd3-fetch';
 
 const mapStateToProps = (state: StateType) => {
@@ -28,8 +28,13 @@ type Props = {} & ReturnType<typeof mapStateToProps> &
 
 type State = {
   storyXMLUrl: string;
+  storyFlower: any;
+  orderInfo: string[][];
+  straightenInfo: any[][];
+  compressInfo: number[][];
   strokes: Path[];
   nodes: number[][][];
+  names: string[];
   strokeWidth: number;
   speed: number;
   opt: String;
@@ -44,8 +49,13 @@ class DrawCanvas extends Component<Props, State> {
     super(props);
     this.state = {
       storyXMLUrl: 'xml/busan.xml',
+      storyFlower: null,
+      orderInfo: [],
+      straightenInfo: [],
+      compressInfo: [],
       strokes: [],
       nodes: [],
+      names: [],
       strokeWidth: 4,
       speed: 300,
       opt: '',
@@ -59,21 +69,11 @@ class DrawCanvas extends Component<Props, State> {
   async componentDidMount() {
     const xmlUrl = this.state.storyXMLUrl;
     const xmlData = await xml(xmlUrl);
-    const storyFlower = storyflow();
-    const storyData = storyFlower.read(xmlData);
-    const generator = storyFlower.extent([[0, 0], [800, 500]]).lineWidth(3);
-    const storyLayout = generator(storyData);
-    const nodes: number[][][] = storyLayout.nodes;
-    nodes.forEach(line => {
-      line.forEach(node => {
-        node[1] /= 50;
-        node[1] += 150;
-        node[0] *= 2;
-      });
-    });
+    const storyFlower = new iStoryline();
     this.setState({
-      nodes: nodes
+      storyFlower: storyFlower
     });
+    storyFlower.read(xmlData);
     this.refresh();
     document.addEventListener('keydown', e => {
       console.log(e);
@@ -93,6 +93,11 @@ class DrawCanvas extends Component<Props, State> {
         this.clearOpt();
         this.setState({
           opt: 'compress'
+        });
+      } else if (e.keyCode == 52) {
+        this.clearOpt();
+        this.setState({
+          opt: 'straighten'
         });
       } else if (e.keyCode == 27) {
         this.clearOpt();
@@ -117,17 +122,91 @@ class DrawCanvas extends Component<Props, State> {
     });
   };
 
+  private findIndexByName = (name: string) => {
+    let res = -1;
+    this.state.names.forEach((str, index) => {
+      if (str == name) {
+        res = index;
+      }
+    });
+    return res;
+  };
+
+  private findIndexByPosition = (x: number, y: number) => {
+    let min_y = -1;
+    let res = -1;
+    this.state.nodes.forEach((line, index) => {
+      line.forEach((node, pos) => {
+        if (node[0] >= x && pos > 0 && line[pos - 1][0] < x) {
+          const start_x = line[pos - 1][0];
+          const start_y = line[pos - 1][1];
+          const end_x = line[pos][0];
+          const end_y = line[pos][1];
+          const pos_y =
+            start_y + ((end_y - start_y) * (x - start_x)) / (end_x - start_x);
+          console.log(pos_y);
+          if ((min_y == -1 || pos_y > min_y) && pos_y < y) {
+            min_y = pos_y;
+            res = index;
+          }
+        }
+      });
+    });
+    console.log(y, min_y);
+    return res;
+  };
+
   private onSortDown = (e: paper.MouseEvent) => {
     const result = project.hitTest(e.point, hitOption);
     if (result) {
       const item = result.item;
       this.setState({
-        selectPath: item
+        selectPath: item,
+        startPosition: e.point
       });
     }
   };
-  private onSortUp = (e: paper.MouseEvent) => {};
-  private onSortDrag = (e: paper.MouseEvent) => {};
+  private onSortUp = (e: paper.MouseEvent) => {
+    if (this.state.selectPath) {
+      if (this.state.currPath) {
+        this.state.currPath.remove();
+        this.setState({
+          currPath: null
+        });
+      }
+      const name = this.state.selectPath.name;
+      const x = e.point.x;
+      const y = e.point.y;
+      const res = this.findIndexByPosition(x, y);
+      if (res == -1) {
+        return;
+      }
+      const before_name = this.state.names[res];
+      if (name == before_name) {
+        return;
+      }
+      this.state.orderInfo.push([before_name, name]);
+      this.refresh();
+    }
+  };
+  private onSortDrag = (e: paper.MouseEvent) => {
+    if (this.state.selectPath) {
+      if (this.state.currPath) {
+        this.state.currPath.remove();
+        this.setState({
+          currPath: null
+        });
+      }
+      if (this.state.startPosition) {
+        const path = this.state.selectPath.clone() as Path;
+        path.strokeColor = 'green';
+        path.position.y += e.point.y - this.state.startPosition.y;
+        this.setState({
+          currPath: path
+        });
+      }
+    }
+  };
 
   private onBlurDown = (e: paper.MouseEvent) => {
     if (this.state.selectPath) {
@@ -157,6 +236,36 @@ class DrawCanvas extends Component<Props, State> {
       }
       console.log(this.state.selectPath);
       console.log(this.state.startPosition, this.state.endPosition);
+    }
+  };
+  private onStraightenUp = (e: paper.MouseEvent) => {
+    if (!this.state.selectPath) {
+      const result = project.hitTest(e.point, hitOption);
+      if (result) {
+        this.setState({
+          selectPath: result.item
+        });
+        result.item.strokeColor = 'red';
+      }
+    } else {
+      this.setState({
+        endPosition: e.point
+      });
+      if (this.state.currPath) {
+        this.state.currPath.remove();
+        this.setState({
+          currPath: null
+        });
+      }
+      const name = this.state.selectPath.name;
+      if (this.state.startPosition && this.state.endPosition) {
+        this.state.straightenInfo.push([
+          name,
+          this.state.startPosition.x * 500,
+          this.state.endPosition.x * 500
+        ]);
+      }
+      this.refresh();
     }
   };
   private onBlurDrag = (e: paper.MouseEvent) => {
@@ -217,7 +326,6 @@ class DrawCanvas extends Component<Props, State> {
   };
 
   private onMouseDown = (e: paper.MouseEvent) => {
-    console.log(this.state.opt);
     switch (this.state.opt) {
       case 'sort':
         this.onSortDown(e);
@@ -227,6 +335,9 @@ class DrawCanvas extends Component<Props, State> {
         break;
       case 'compress':
         this.onCompressDown(e);
+        break;
+      case 'straighten':
+        this.onBlurDown(e);
         break;
       default:
         break;
@@ -244,6 +355,8 @@ class DrawCanvas extends Component<Props, State> {
       case 'compress':
         this.onCompressUp(e);
         break;
+      case 'straighten':
+        this.onStraightenUp(e);
       default:
         break;
     }
@@ -262,25 +375,47 @@ class DrawCanvas extends Component<Props, State> {
       case 'compress':
         this.onCompressDrag(e);
         break;
+      case 'straighten':
+        this.onBlurDrag(e);
+        break;
       default:
         break;
     }
   };
 
-  refresh() {
+  async refresh() {
+    const graph = this.state.storyFlower.layout(
+      this.state.orderInfo,
+      this.state.straightenInfo,
+      this.state.compressInfo
+    );
+    const nodes: number[][][] = graph.nodes;
+    const names: string[] = graph.names;
+    nodes.forEach(line => {
+      line.forEach(node => {
+        node[1] /= 50;
+        node[1] += 150;
+        node[0] /= 500;
+      });
+    });
+    this.setState({
+      nodes: nodes,
+      names: names
+    });
     this.state.strokes.forEach(path => {
       path.remove();
     });
     this.setState({
       strokes: []
     });
-    this.state.nodes.forEach(line => {
-      this.appearing(line, this.state.speed);
+    nodes.forEach((line, index) => {
+      this.appearing(line, this.state.speed, names[index]);
     });
   }
 
-  appearing(line: number[][], speed: number) {
+  appearing(line: number[][], speed: number, name: string) {
     const path = new Path();
+    path.name = name;
     path.strokeColor = 'black';
     path.strokeWidth = this.state.strokeWidth;
     this.setState({
