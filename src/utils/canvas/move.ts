0f10da9335fa38abbdc7e6353775b954c3356ed2
zangfenziang <zangfenziang@126.com>
2019-08-15 14:41:22 +0800

    add rotate for Raster

--- src/utils/canvas/move.ts
+++ src/utils/canvas/move.ts
@@ -1,25 +1,58 @@
 import { StoryUtil } from '../util';
 import { IHitOption, StoryGraph } from '../../types';
-import { Segment, project, Matrix, Point, HitResult, Raster } from 'paper';
+import {
+  Segment,
+  project,
+  Matrix,
+  Point,
+  HitResult,
+  Raster,
+  Group,
+  Path
+} from 'paper';
+import { BLACK } from '../color';
 
 export default class MoveUtil extends StoryUtil {
   isMoveShape: boolean;
   isMoveSegment: boolean;
   selectSegment: Segment | null;
   isScale: boolean;
+  isRotate: boolean;
   matrix: Matrix | null;
+  rotateShape: Group | null;
   static deltaPixel = 3;
   constructor(hitOption: IHitOption) {
     super(hitOption);
     this.isMoveShape = false;
     this.isMoveSegment = false;
     this.isScale = false;
+    this.isRotate = false;
     this.selectSegment = null;
     this.matrix = null;
+    this.rotateShape = null;
   }
 
   updateStoryStore(graph: StoryGraph) {
     super.updateStoryStore(graph);
+  }
+
+  createRotateShape(center: Point, point: Point = new Point(0, -15)) {
+    const group = new Group();
+    const circle = new Path.Circle(new Point(0, 0), 3);
+    circle.strokeColor = BLACK;
+    circle.strokeWidth = 1;
+    const clickCircle = new Path.Circle(point, 3);
+    clickCircle.fillColor = BLACK;
+    const path = new Path();
+    path.add(new Point(0, 0));
+    path.add(point);
+    path.strokeWidth = 1;
+    path.strokeColor = BLACK;
+    group.addChild(circle);
+    group.addChild(clickCircle);
+    group.addChild(path);
+    group.translate(center);
+    return group;
   }
 
   down(e: paper.MouseEvent) {
@@ -28,10 +61,23 @@ export default class MoveUtil extends StoryUtil {
       if (hitRes) {
         switch (hitRes.type) {
           case 'fill':
-            this.selectPath = hitRes.item;
-            this.isMoveShape = true;
-            this.selectSegment = null;
-            this.isMoveSegment = false;
+            if (
+              hitRes.item &&
+              hitRes.item.parent == this.rotateShape &&
+              this.selectPath &&
+              this.selectPath.matrix
+            ) {
+              this.matrix = this.selectPath.matrix.clone();
+              this.isRotate = true;
+              this.selectSegment = null;
+              this.isMoveSegment = false;
+              this.isMoveShape = true;
+            } else {
+              this.selectPath = hitRes.item;
+              this.isMoveShape = true;
+              this.selectSegment = null;
+              this.isMoveSegment = false;
+            }
             break;
           case 'pixel':
             this.selectPath = hitRes.item;
@@ -91,9 +137,21 @@ export default class MoveUtil extends StoryUtil {
   move(e: paper.MouseEvent) {
     if (project && e.point) {
       project.activeLayer.selected = false;
+      this.selectPath = null;
+      if (this.rotateShape) {
+        this.rotateShape.remove();
+        this.rotateShape = null;
+      }
       const hitRes = project.hitTest(e.point, this.hitOption);
       if (hitRes && hitRes.item) {
         hitRes.item.selected = true;
+        if (hitRes.item instanceof Raster) {
+          const raster = hitRes.item;
+          if (raster.position) {
+            this.rotateShape = this.createRotateShape(raster.position);
+            this.selectPath = raster;
+          }
+        }
       }
     }
   }
@@ -104,6 +162,11 @@ export default class MoveUtil extends StoryUtil {
     this.isMoveShape = false;
     this.isMoveSegment = false;
     this.isScale = false;
+    this.isRotate = false;
+    if (this.rotateShape) {
+      this.rotateShape.remove();
+      this.rotateShape = null;
+    }
   }
 
   drag(e: paper.MouseEvent) {
@@ -111,17 +174,48 @@ export default class MoveUtil extends StoryUtil {
       if (e.delta && e.point) {
         if (this.isScale) {
           if (this.startPosition && this.selectPath.position) {
-            const matrix = this.selectPath.matrix;
             const scale = e.point
               .subtract(this.selectPath.position)
               .divide(this.startPosition.subtract(this.selectPath.position));
-            if (scale.x && scale.y && matrix && this.matrix) {
-              const mat = new Matrix(scale.x, 0, 0, scale.y, 0, 0);
+            if (scale.x && scale.y && this.matrix) {
+              const mat = this.matrix.clone();
+              if (
+                mat.a != null &&
+                mat.b != null &&
+                mat.c != null &&
+                mat.d != null
+              ) {
+                mat.a *= scale.x;
+                mat.c *= scale.x;
+                mat.b *= scale.y;
+                mat.d *= scale.y;
+              }
+              this.selectPath.matrix = mat;
+            }
+          }
+        } else if (this.isRotate) {
+          if (this.selectPath.position) {
+            const center = this.selectPath.position;
+            const delta = e.point.subtract(center);
+            if (this.rotateShape) {
+              this.rotateShape.remove();
+              this.rotateShape = this.createRotateShape(center, delta);
+            }
+            if (this.matrix && delta.x && delta.y) {
+              const mat = new Matrix();
+              let angle = (Math.atan(-delta.x / delta.y) / Math.PI) * 180;
+              if (delta.y > 0) {
+                angle += 180;
+              }
+              mat.rotate(angle, new Point(0, 0));
               this.selectPath.matrix = mat.prepend(this.matrix);
             }
           }
         } else {
           this.selectPath.translate(e.delta);
+          if (this.rotateShape) {
+            this.rotateShape.translate(e.delta);
+          }
         }
       }
     }
